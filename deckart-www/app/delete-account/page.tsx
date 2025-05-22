e5d0b4588b0b7c5025ac8a9d55a0f285d07a6d28
cfoltran <clement.foltran@protonmail.com>
2025-05-22 16:13:53 +0200

    training + delete account

---
+++ deckart-www/app/delete-account/page.tsx
@@ -0,0 +1,14 @@
+export default function DeleteAccount() {
+  return (
+    <section className="container" style={{ paddingTop: '100px' }}>
+      <h1>Supprimer mon compte</h1>
+      <main>
+        <p>Vous souhaitez supprimer votre compte ?</p>
+        <p>Vous pouvez le faire en envoyant un e-mail à l'adresse suivante :</p>
+        <p>
+          <a href="mailto:contact@deckart.xyz">contact@deckart.xyz</a>
+        </p>
+      </main>
+    </section>
+  );
+} 
---
+++ deckart-www/app/training/page.tsx
@@ -0,0 +1,22 @@
+export default function Training() {
+  return (
+    <section className="container" style={{ paddingTop: '100px' }}>
+      <main>
+        <iframe 
+          width="540" 
+          height="305" 
+          src="https://sibforms.com/serve/MUIFAP41AhIi5HrQmWLsSHjdpvDhe5Z8R0HbrryO9bw3_dRtpb2dTO3xj_AHAoKemcrK8ya6yk5c0epRcDtjWFRTPX3dyxUxPHPhPuM_Gcf36ihDO5yECgy18dCgv3I5bRFoaMVwOGqZVGh3XNXn58CT-pbHCVI1-VN4DQP3J-Ne4IY4IJWyCDGYkpBuX-oWSdTfDYs3MoHkkqvV" 
+          frameBorder="0" 
+          scrolling="auto" 
+          allowFullScreen 
+          style={{ 
+            display: 'block',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            maxWidth: '100%'
+          }}
+        />
+      </main>
+    </section>
+  );
+} 
